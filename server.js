fd1922d96c033197b3361e5eb3350dbaa276096b
level <joahas@posteo.de>
2026-09-16 15:00:46 +0200

    intigrated express

--- server.js
+++ server.js
@@ -2,5 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get("/", (request, response) => {
+  response.render("index", {
+    // data: data,
+  });
+});
